60246b00e9cd271a474b80d6ad03ad38e6ed774e
belfa401-bit <flbelfa401@gmail.com>
2026-04-15 20:10:24 +0700

    LatihanPolimorfisme2

--- Latihan_Polimorfisme2/Program.cs
+++ Latihan_Polimorfisme2/Program.cs
@@ -1,2 +1,65 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class MetodePembayaran
+{
+    public virtual void Bayar(int jumlah)
+    {
+        Console.WriteLine("Memproses pembayaran...");
+    }
+}
+
+class BankTransfer : MetodePembayaran
+{
+    public override void Bayar(int jumlah)
+    {
+        Console.WriteLine("Silahkan transfer sejumlah Rp " + jumlah + " ke nomor 12345678");
+    }
+}
+
+class EWallet : MetodePembayaran
+{
+    public override void Bayar(int jumlah)
+    {
+        Console.WriteLine("Saldo terpotong sejumlah Rp " + jumlah);
+    }
+}
+
+class KartuKredit : MetodePembayaran
+{
+    public override void Bayar(int jumlah)
+    {
+        Console.WriteLine("Transaksi sejumlah Rp " + jumlah + " berhasil dilakukan");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("-----Simulasi Checkout-----");
+        Console.WriteLine("Pilih metode:");
+        Console.WriteLine("1. Bank Transfer");
+        Console.WriteLine("2. E-Wallet");
+        Console.WriteLine("3. Kartu Kredit");
+
+        Console.Write("Pilihan Anda (1/2/3): ");
+        int pilihan = int.Parse(Console.ReadLine());
+
+        int jumlah = 100000;
+        MetodePembayaran pembayaran = null;
+
+        if (pilihan == 1)
+            pembayaran = new BankTransfer();
+        else if (pilihan == 2)
+            pembayaran = new EWallet();
+        else if (pilihan == 3)
+            pembayaran = new KartuKredit();
+        else
+        {
+            Console.WriteLine("Pilihan tidak valid");
+            return;
+        }
+
+        pembayaran.Bayar(jumlah);
+    }
+}
